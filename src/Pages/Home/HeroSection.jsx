641c848806820d177111446d33d861cb5cafd8b5
Vishal Saini <vishalsaini092@gmail.com>
2023-09-13 13:59:55 +0530

    first commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Vishal Saini</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          I'm a passionate Full-stack Web Developer
+            <br /> With a strong foundation in both front-end and back-end development
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,19 @@
+import AboutMe from "../AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
+import MySkills from "../MySkills";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <MySkills />
+      <AboutMe />
+      <MyPortfolio />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
