b91c921497f7858d463600cf04c933588f40a921
Vishal Saini <vishalsaini092@gmail.com>
2023-09-30 19:17:50 +0530

    update

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -17,7 +17,7 @@ export default function HeroSection() {
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+        <img src="./img/about-me.jpg" alt="Hero Section" />
       </div>
     </section>
   );
